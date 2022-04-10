97526b9c8f9fc4f8781d5f82dee1f8d8b7ffa0de
Raphael Braga <rbragamouraa@gmail.com>
2022-04-10 20:53:08 -0300

    Aleatoriedade

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 let click2;
 let click1;
 
+//Quantificação de cartas solicitadas
 let quantidadeCards = Number(prompt("Com quantas cartas você quer jogar?"));
 
 while(quantidadeCards < 4 || quantidadeCards > 14 || quantidadeCards % 2 != 0){
     quantidadeCards = Number(prompt("Com quantas cartas você quer jogar?"));
 }
+//Quantificação de cartas solicitadas
 
+//Aleatoriedade das imagens de verso
 const fundoCartas = ["bobrossparrot.gif", 
                     "explodyparrot.gif", 
                     "fiestaparrot.gif", 
@@ -28,9 +31,9 @@ fundoRandom.sort(comparador);
 function comparador() { 
 return Math.random() - 0.5; 
 }
+//Aleatoriedade das imagens de verso
 
-console.log(fundoRandom)
-
+//Invocação da cartas solicitadas
 function gerarCartas() {
     let cards = document.querySelector(".cards");
     console.log(cards)
@@ -43,26 +46,45 @@ function gerarCartas() {
     }
 }
 gerarCartas();
+//Invocação da cartas solicitadas
 
+function virar(elemento) {
+    console.log(elemento);
 
-function virar(elemento){
-    if (click1 === undefined){
+    if (click1 === undefined) {
+        
+        let imagemDetras = elemento.querySelector(".back-face");
+
+        
         elemento.querySelector(".front-face").classList.add("rodar-front-face");
-        let versoImagem = elemento.querySelector(".back-face").classList.add("desrodar-back-face");
-        click1 = versoImagem.src
+        imagemDetras.classList.add("desrodar-back-face");
+
+        click1 = imagemDetras.src;
     }
     else {
-        elemento.querySelector(".front-face").classList.add("rodar-front-face");
-        let versoImagem = elemento.querySelector(".back-face").classList.add("desrodar-back-face");
-        click2 = versoImagem.src
-    }
+        let imagemDetras = elemento.querySelector(".back-face");
 
-    if (click1 === click2) {
-        console.log("São iguais, mantenha a carta virada");
+        
+        elemento.querySelector(".front-face").classList.add("rodar-front-face");
+        imagemDetras.classList.add("desrodar-back-face");
+
+        click2 = imagemDetras.src;
+        
+        if (click1 === click2) {
+            console.log("São iguais, mantenha a carta virada");
+            delete click1
+            delete click2
+            console.log(click1)
+        }
+        else {
+            console.log("Diferentes, desvire as cartas")
+            delete click1
+            delete click2
+        }
     }
-    else {
-        console.log("Diferentes, desvire as cartas")
-    }
+    
+
 }
+
 
 
