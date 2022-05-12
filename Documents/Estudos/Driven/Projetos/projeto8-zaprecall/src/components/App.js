c8b0ec0ddfcdaf22a8b1a0b38ce19d40a210cb26
Raphael Braga <rbragamouraa@gmail.com>
2022-05-12 09:42:28 -0300

    alfa

---
+++ Documents/Estudos/Driven/Projetos/projeto8-zaprecall/src/components/App.js
@@ -0,0 +1,17 @@
+import "../css/reset.css"
+import "../css/estilo.css"
+
+
+
+
+export default function App(){
+    return(
+        <>
+        <div className="tela1">
+        <img className="logo" src="../images/logo.png" />
+        <h1>ZapRecall</h1>
+        </div>
+        </>
+
+    )
+}
