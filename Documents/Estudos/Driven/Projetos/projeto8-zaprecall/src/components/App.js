227773f861a39a8191c3f57493d14979c0638ca5
Raphael Braga <rbragamouraa@gmail.com>
2022-05-15 09:31:36 -0300

    Start

--- Documents/Estudos/Driven/Projetos/projeto8-zaprecall/src/components/App.js
+++ Documents/Estudos/Driven/Projetos/projeto8-zaprecall/src/components/App.js
@@ -1,16 +1,33 @@
+
 import "../css/reset.css"
 import "../css/estilo.css"
+import Screen1 from "./Screen1"
+import Screen2 from "./Screen2"
+import Footer from "./Footer"
+import Transition from "./shared/Transition"
 
 
 
 
 export default function App(){
+    const [hidden1, setHidden1] = React.useState("tela1")
+    const [hidden2, setHidden2] = React.useState("hidden")
+    console.log(hidden1)
+    console.log(hidden2)
+
+     if(hidden1 === "tela1" && hidden2 === "hidden"){
+        setHidden1("hidden")
+        setHidden2("tela2")
+        console.log(hidden1)
+        console.log(hidden2)
+     }
+    
+
     return(
         <>
-        <div className="tela1">
-        <img className="logo" src="../images/logo.png" />
-        <h1>ZapRecall</h1>
-        </div>
+       <Screen1 />
+       <Screen2 />
+       <Footer />
         </>
 
     )
